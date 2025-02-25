c8a396b474166969bcfa9da23c37fd00fb20e8e9
FrancoA56 <franco.adamoli@gmail.com>
2025-02-25 11:34:00 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// tailwind.config.js
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        a: "#3490dc",
+        b: "#ffed4a",
+        c: "#ffed4a",
+        d: "#ffed4a",
+        error: "#e3342f",
+      },
+      fontSize: {
+        xs: ".75rem",
+        sm: ".875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
